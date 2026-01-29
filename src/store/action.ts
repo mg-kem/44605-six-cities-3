45c5f2c8596499c85d242b5e5a9b4474906640cb
Дмитрий Гилёв <kem-mit@yandex.ru>
2026-01-29 13:50:24 +0700

    Добавил примитивный reducer и action

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,7 +1,12 @@
 import { createAction } from '@reduxjs/toolkit';
+import { ICity, IOffer } from '../types/types';
 
 /** Действие для изменения города */
-export const changeCity = createAction('changeCity');
+export const changeCity = createAction('changeCity', (city: ICity) => ({
+  payload: city,
+}));
 
 /** Действие для заполнения списка предложений */
-export const fillingOffers = createAction('fillingOffers');
+export const fillingOffers = createAction('fillingOffers', (offers: IOffer[]) => ({
+  payload: offers,
+}));
--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,6 +1,7 @@
 import { createReducer } from '@reduxjs/toolkit';
 import { Offers } from '../mock/offers';
 import { Cities } from '../const/cities';
+import { changeCity, fillingOffers } from './action';
 
 // Определение начального состояния приложения
 const initialState = {
@@ -8,3 +9,30 @@ const initialState = {
   offers: Offers,
 };
 
+const reducer = createReducer(initialState,
+  (builder) => {
+    builder
+      .addCase(changeCity,
+        (state, action) => {
+          state.city = action.payload.title;
+        })
+      .addCase(fillingOffers,
+        (state, action) => {
+          state.offers = action.payload;
+        });
+  }
+);
+
+// Описание редюсера
+// CreateReducer - создает редюсер
+// builder - builder - это объект, который позволяет добавлять новые случаи в редюсер
+// addCase - добавляет новый случай в редюсер
+// state - состояние
+// action - действие
+// return state - возвращает новое состояние
+// return state.city = action.payload.title; - изменяет состояние на новое
+// return state.offers = action.payload; - изменяет состояние на новое
+// return state; - возвращает новое состояние
+// За счет того, что redux toolkit использует immer, мы можем изменять состояние напрямую, без необходимости возвращать новое состояние
+
+export default reducer;
